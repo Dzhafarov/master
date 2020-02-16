5562a6cd4f74ad4e3652bf9d491f41172202625b
ResaterJke <ildardjafarov2@gmail.com>
2020-02-16 18:19:53 +0200

    master

--- src/main/java/Method.java
+++ src/main/java/Method.java
@@ -2,4 +2,7 @@ public class Method {
     public void doSomeStuff(){
         System.out.println("1st method");
     }
+    public void doSomeStuffM(){
+        System.out.println("1st method");
+    }
 }
